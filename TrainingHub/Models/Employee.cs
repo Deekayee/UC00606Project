7297b4a996b7b91b1e05c46a63bcfcd4473e4e7b
Deekayee <dmytro.k.nesterenko@gmail.com>
2025-12-11 23:08:12 +0000

    Implemented constructors and methods #25

--- TrainingHub/Models/Employee.cs
+++ TrainingHub/Models/Employee.cs
@@ -4,6 +4,7 @@ namespace TrainingHub.Models;
 
 public abstract class Employee
 {
+    // Properties
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -13,4 +14,37 @@ public abstract class Employee
     public DateTime ContractEndDate { get; set; }
     public DateTime CriminalRecordEndDate { get; set; }
     public decimal SalaryBase { get; set; }
+
+    // Constructors
+
+    // Default constructor
+    public Employee() { }
+
+    // Parameterized constructor
+    public Employee(int id, string firstName, string lastName, string address, string phoneNumber, DateTime contractStartDate, DateTime contractEndDate, DateTime criminalRecordEndDate, decimal salaryBase)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Address = address;
+        PhoneNumber = phoneNumber;
+        ContractStartDate = contractStartDate;
+        ContractEndDate = contractEndDate;
+        CriminalRecordEndDate = criminalRecordEndDate;
+        SalaryBase = salaryBase;
+    }
+
+    // Methods
+
+    // Method to check if the contract is valid
+    public bool IsContractValid => ContractEndDate < DateTime.Now;
+
+    // Abstract method to calculate the salary, the implementation is left to the derived classes
+    public abstract decimal CalculateMonthlySalary();
+
+    // Method to check if the employee has an expired criminal record
+    public bool HasExpiredCriminalRecord => CriminalRecordEndDate < DateTime.Now;
+
+    // Abstract to string method
+    public abstract override string ToString();
 }
