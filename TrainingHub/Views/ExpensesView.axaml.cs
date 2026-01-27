3db1efa4a8dfa98da7bcb887d683e822623e5209
Fabiana Dorado <fdpvieira@gmail.com>
2026-01-27 20:54:49 +0000

    Add login command and screen toggle #50

---
+++ TrainingHub/Views/ExpensesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TrainingHub.Views;
+
+public partial class ExpensesView : UserControl
+{
+    public ExpensesView()
+    {
+        InitializeComponent();
+    }
+}
