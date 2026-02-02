235814f6404087e978fbc69913dd2197c2318584
Deekayee <dmytro.k.nesterenko@gmail.com>
2026-02-02 01:51:21 +0000

    using the modal

--- TrainingHub/App.axaml.cs
+++ TrainingHub/App.axaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using Avalonia.Markup.Xaml;
 using TrainingHub.ViewModels;
 using TrainingHub.Views;
+using TrainingHub.Services;
 
 namespace TrainingHub;
 
@@ -23,9 +24,10 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
+            var dialogService = new DialogService();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = new MainWindowViewModel(dialogService),
             };
         }
 
