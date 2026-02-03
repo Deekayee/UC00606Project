020a3e2fa72481a4bb4ace7e402e14dc8a534b16
Maria Serrano <mariahcserrano@gmail.com>
2026-02-03 11:58:57 +0000

    edit employee view and window logic

---
+++ TrainingHub/Views/EditEmployeeWindow.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using TrainingHub.ViewModels;
+
+namespace TrainingHub.Views
+{
+    public partial class EditEmployeeWindow : Window
+    {
+        public EditEmployeeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCancel(object? sender, RoutedEventArgs e)
+        {
+            Close(false);
+        }
+
+        private void OnSave(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is EditEmployeeViewModel viewModel)
+            {
+                viewModel.UpdateEmployee();
+                Close(true);
+            }
+        }
+    }
+}
